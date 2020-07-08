cd485ea7ef7a65ea0357ae7f0a9cec61d3d13c14
ildebrando113 <67147058+ildebrando113@users.noreply.github.com>
2020-07-08 17:35:20 +0200

    jUnitTest

--- PSMS/main/it/enaip/stage/test/Junit_DB_TEST2.java
+++ PSMS/main/it/enaip/stage/test/Junit_DB_TEST2.java
@@ -2,6 +2,9 @@ package it.enaip.stage.test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.Connection;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -12,6 +15,7 @@ import org.junit.jupiter.api.Test;
 import it.enaip.stage.dao.DaoUser;
 import it.enaip.stage.dao.DataSourceFactory;
 import it.enaip.stage.model.User;
+import it.enaip.stage.model.User.Status;
 
 
 class Junit_DB_TEST2 {
@@ -30,46 +34,57 @@ class Junit_DB_TEST2 {
 	
 	void testGetMaxId() throws Exception {
 		System.out.println("test MAX id");{
-	  //  Connection conn = DataSourceFactory.getConnection();
-	     DaoStuff dao= DaoStuff.getInstance();
-	     int id = dao.getMaxIndex();
+	  
+	     DaoUser user= DaoUser.getInstance();
+	     int id = user.getMaxIndex();
 	     
-	     assertEquals(id - dao.getMaxIndex()  , 0);
+	     assertEquals(id - user.getMaxIndex()  , 0);
 	     }
 	}
-	
+	/*
 	@Test
 	void testUpdate() throws Exception {
-		boolean rowup= false;
-		DaoStuff dao= DaoStuff.getInstance();
-		Stuff stuff = new Stuff(2,"Update", "test", 1, "test");
+		String date = "01/04/2001";
+		Date data = new SimpleDateFormat("dd/MM/yyyy").parse(date);
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		Date parsedDate = dateFormat.parse("10/05/1980 17:30");
+		Timestamp creationtime = new java.sql.Timestamp(parsedDate.getTime());
+
 		
-		rowup=dao.update(stuff);
+		boolean rowup= false;
+		DaoUser userTest= DaoUser.getInstance();
+		User user = new User(3, "pako", "TestUpdate",data,creationtime,19,Status.C);
+		
+		rowup=userTest.update(user);
 		assertEquals(true, rowup);
 		
 	}
-	
 	*/
+	
 	@Test
 	void testSave()  throws Exception{
 		boolean rowsave= false;
+		String date = "01/04/2001";
+		Date data = new SimpleDateFormat("dd/MM/yyyy").parse(date);
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		Date parsedDate = dateFormat.parse("10/05/1980 17:30");
+		Timestamp creationtime = new java.sql.Timestamp(parsedDate.getTime());
+		DaoUser userTest = DaoUser.getInstance();
 		
-		DaoUser dao = DaoUser.getInstance();
-		
-		User stuff = new User(0, "test-new", "test-desc",1, "test-loc");
-		rowsave=dao.save(stuff);
+		User user = new User(0, "Testperico", "Testcotorra",data,creationtime,29,Status.S);
+		rowsave=userTest.save(user);
 		assertEquals(true, rowsave);
 	}
-	/*
+
 	@Test
 	void deleteTest() throws Exception {
 	
 		boolean rowdel= false;
-		DaoStuff dao = DaoStuff.getInstance();
-		Stuff stuff = new Stuff(3);
-		rowdel= dao.delete(stuff);
+		DaoUser userTest = DaoUser.getInstance();
+		User user = new User(3);
+		rowdel= userTest.delete(user);
 		assertEquals(true, rowdel);
 		
 	}
-	*/
+	
 }
